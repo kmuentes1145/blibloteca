fafe4ef2b41882a72d0859d2f460a7f854d40deb
kelvin <kmuentes1145@utm.edu.ec>
2026-07-28 08:14:07 -0500

    cambio

--- backend/src/libros/libro.module.ts
+++ backend/src/libros/libro.module.ts
@@ -1,4 +1,6 @@
--//aqui se define el módulo Libro, que contiene los componentes relacionados con los libros.
+// Este archivo define un módulo de NestJS llamado `LibroModule` que se encarga de gestionar la funcionalidad relacionada con los libros en una biblioteca virtual.
+// El módulo importa `TypeOrmModule` para interactuar con la base de datos a través de la entidad `Libro`.
+// Además, proporciona el servicio `LibroService` y el controlador `LibroController` para manejar la lógica de negocio y las rutas HTTP relacionadas con los libros.
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Libro } from './libro.entity';
